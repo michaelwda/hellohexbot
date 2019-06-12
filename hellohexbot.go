b77ffc407ced753052ad22cb060ec17730ab2b78
Michael <michael@ptccinc.com>
2019-06-12 13:32:56 -0400

    adds opengl sprite rendering

--- hellohexbot.go
+++ hellohexbot.go
@@ -3,73 +3,135 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"image"
 	"io/ioutil"
+	"log"
+	"math/rand"
 	"net/http"
-	"sync"
 	"time"
+
+	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/pixelgl"
+	"github.com/lucasb-eyer/go-colorful"
+	"golang.org/x/image/colornames"
 )
 
-const url = "https://api.noopschallenge.com/hexbot"
+const (
+	url         = "https://api.noopschallenge.com/hexbot"
+	height      = 500
+	width       = 500
+	pixelWidth  = 50
+	pixelHeight = 50
+)
+
+type colorUpdate struct {
+	x   int
+	y   int
+	w   int
+	h   int
+	hex string
+}
 
 func main() {
-
 	fmt.Printf("Hello HexBot!\n")
+	pixelgl.Run(run)
+}
 
-	//create a waitgroup to sync my network goroutines
-	var wg sync.WaitGroup
+func run() {
 
-	//make a buffered channel of 100 strings
-	messages := make(chan string, 100)
+	//set up our window
+	cfg := pixelgl.WindowConfig{
+		Title:  "HexBot!",
+		Bounds: pixel.R(0, 0, 300, 300),
+		VSync:  true,
+	}
+	win, err := pixelgl.NewWindow(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	//initially make it blue
+	win.Clear(colornames.Skyblue)
 
 	//make a rate-limiter
-	//1 request per 50 milliseconds, 20 requests a second, our 100 requests should take about 5 seconds to complete
 	limiter := make(chan time.Time, 1)
 
-	//on-tick- add a value to the channel
+	//on-tick- add a value to the channel - only 10 requests a second
 	go func() {
-		for t := range time.Tick(50 * time.Millisecond) {
+		for t := range time.Tick(100 * time.Millisecond) {
 			limiter <- t
 		}
 	}()
 
-	//spawn 100 goroutines, passing in my rate limiter and the syncgroup
-	for i := 0; i < 100; i++ {
-		wg.Add(1)
-		go CallApi(messages, limiter, &wg)
+	//make a buffered channel of 100 updates
+	updates := make(chan colorUpdate, 100)
+
+	//spawn 50 goroutines, passing in my rate limiter
+	for i := 0; i < 50; i++ {
+		go callApi(updates, limiter)
 	}
 
-	//wait for all routines to return
-	wg.Wait()
+	for !win.Closed() {
+		win.Update()
 
-	//receive all from channel
-	for i := 0; i < 100; i++ {
-		msg := <-messages
-		fmt.Println(msg)
+		//read an update
+		select {
+		case update := <-updates:
+			{
+				fmt.Println("received update")
+				sprite := createSpriteFromHex(update.hex, update.w, update.h)
+				sprite.Draw(win, pixel.IM.Moved(pixel.V(float64(update.x), float64(update.y))))
+			}
+		default:
+			{
+				//do nothing, continue to render loop
+			}
+		}
 	}
-
 }
 
-func CallApi(messages chan string, limiter chan time.Time, wg *sync.WaitGroup) {
-	<-limiter
-	fmt.Println("request", time.Now())
-	response, err := http.Get(url)
+func createSpriteFromHex(hex string, w int, h int) *pixel.Sprite {
+	c, err := colorful.Hex(hex)
 	if err != nil {
-		messages <- err.Error()
-	} else {
-		body, _ := ioutil.ReadAll(response.Body)
-		messages <- GetHex(body)
+		log.Fatal(err)
 	}
-	wg.Done()
+	img := image.NewRGBA(image.Rect(0, 0, w, h))
+	for i := 0; i < 5; i++ {
+		for j := 0; j < 5; j++ {
+			img.Set(i, j, c)
+		}
+	}
+	pic := pixel.PictureDataFromImage(img)
+	sprite := pixel.NewSprite(pic, pic.Bounds())
+
+	return sprite
 }
 
-func GetHex(body []byte) string {
+func callApi(updates chan colorUpdate, limiter chan time.Time) {
+	//repeatedly call the api and get colors, with respect to the limiter
+	for true {
+		<-limiter
+		response, err := http.Get(url)
+		if err == nil {
+			body, _ := ioutil.ReadAll(response.Body)
+			update := colorUpdate{
+				x:   rand.Intn(width),
+				y:   rand.Intn(height),
+				w:   rand.Intn(pixelWidth-1) + 1,
+				h:   rand.Intn(pixelHeight-1) + 1,
+				hex: getHex(body),
+			}
+			updates <- update
+		}
+	}
+}
+
+func getHex(body []byte) string {
 	var data map[string]interface{}
 	//var assignment not allowed inside an if. must use := shorthand
 	//inlining the nil check using chaining
-	//interestingly, if you don't use K&R bracket style here it gives a syntax error?
 	if err := json.Unmarshal(body, &data); err != nil {
-		fmt.Printf("The HTTP request failed with error %s\n", err)
-		panic(err)
+		return "#000000"
 	}
 	colors := data["colors"].([]interface{})
 	firstColor := colors[0].(map[string]interface{})
