f0492cccdd27f288dcb38a92b0e99b80d7ec633b
Michael <michael@ptccinc.com>
2019-06-12 10:27:40 -0400

    Spawns 100 gorountines coordinated with a waitgoup and rate-limiter

--- hellohexbot.go
+++ hellohexbot.go
@@ -5,25 +5,64 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"sync"
+	"time"
 )
 
 const url = "https://api.noopschallenge.com/hexbot"
 
 func main() {
+
 	fmt.Printf("Hello HexBot!\n")
-	response, err := http.Get(url)
-	if err != nil {
-		fmt.Printf("The HTTP request failed with error %s\n", err)
-	} else {
-		body, _ := ioutil.ReadAll(response.Body)
-		hex := GetHex(body)
-		fmt.Printf("My hex value is: %s", hex)
+
+	//create a waitgroup to sync my network goroutines
+	var wg sync.WaitGroup
+
+	//make a buffered channel of 100 strings
+	messages := make(chan string, 100)
+
+	//make a rate-limiter
+	//1 request per 50 milliseconds, 20 requests a second, our 100 requests should take about 5 seconds to complete
+	limiter := make(chan time.Time, 1)
+
+	//on-tick- add a value to the channel
+	go func() {
+		for t := range time.Tick(50 * time.Millisecond) {
+			limiter <- t
+		}
+	}()
+
+	//spawn 100 goroutines, passing in my rate limiter and the syncgroup
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go CallApi(messages, limiter, &wg)
+	}
+
+	//wait for all routines to return
+	wg.Wait()
+
+	//receive all from channel
+	for i := 0; i < 100; i++ {
+		msg := <-messages
+		fmt.Println(msg)
 	}
 
 }
 
-func GetHex(body []byte) string {
+func CallApi(messages chan string, limiter chan time.Time, wg *sync.WaitGroup) {
+	<-limiter
+	fmt.Println("request", time.Now())
+	response, err := http.Get(url)
+	if err != nil {
+		messages <- err.Error()
+	} else {
+		body, _ := ioutil.ReadAll(response.Body)
+		messages <- GetHex(body)
+	}
+	wg.Done()
+}
 
+func GetHex(body []byte) string {
 	var data map[string]interface{}
 	//var assignment not allowed inside an if. must use := shorthand
 	//inlining the nil check using chaining
